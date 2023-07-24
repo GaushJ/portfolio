ebe348929f43b7b6e5b3e5d0dc7b059174314062
Gauransh Juneja <gauranshjuneja@gmail.com>
2023-07-24 17:56:31 +0530

    fixes

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -18,22 +18,24 @@ const projectVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
-const Project = ({ title, image }) => {
+const Project = (props) => {
+  const { title, image, description,link } = props
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
     bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
 
 
   return (
-    <motion.div variants={projectVariant} className="relative">
-      <div className={overlayStyles}>
-        <p className="text-2xl font-playfair">{title}</p>
-        <p className="mt-7">
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-          porttitor accumsan tincidunt.
-        </p>
-      </div>
-      <img src={image} alt={title} />
-    </motion.div>
+    <a href={link} target="_blank">
+      <motion.div variants={projectVariant} className="relative">
+        <div className={overlayStyles}>
+          <p className="text-2xl font-playfair">{title}</p>
+          <p className="mt-7">
+            {description}
+          </p>
+        </div>
+        <img src={image} alt={title} />
+      </motion.div>
+    </a>
   );
 };
 
@@ -61,9 +63,7 @@ const Projects = () => {
           </div>
         </div>
         <p className="mt-10 mb-10">
-          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
-          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
-          purus nec eget eleifend ut elit.
+          Below are some of the works that I performed during my internships and freelancing.
         </p>
       </motion.div>
 
@@ -83,17 +83,17 @@ const Projects = () => {
           >
             BEAUTIFUL USER INTERFACES
           </div>
-          <Project title="Eventmender" image={eventmender}/>
-          <Project title="GoodGameNation" image={ggnation}/>
+          <Project title="Eventmender" link={"https://www.eventmender.com"} image={eventmender} description={"Developed using MERN stack and redux for state management along with admin dashboard."} />
+          <Project title="GoodGameNation" link={"https://ggnation.com/"} image={ggnation} description={"Gaming webapp using ReactJS and redux for state management along with admin dashboard."} />
 
           {/* ROW 2 */}
-          <Project title="Offiql" image={offiql}/>
-          <Project title="Project 4" />
+          <Project title="Offiql" image={offiql} link={"https://www.offiqltech.com"}/>
+          {/* <Project title="Project 4" />
           <Project title="Project 5" />
 
           {/* ROW 3 */}
-          <Project title="Project 6" />
-          <Project title="Project 7" />
+          {/* <Project title="Project 6" />
+          <Project title="Project 7" /> */} 
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
